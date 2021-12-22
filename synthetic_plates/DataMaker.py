db0209cbc6fdaeaf5a5990d0854f61622bb26687
Ruhallah93 <ruhallah.ahmadian@gmail.com>
2021-12-22 23:01:27 +0330

    Glyphs:  - generate glyphs

--- synthetic_plates/DataMaker.py
+++ synthetic_plates/DataMaker.py
@@ -7,17 +7,21 @@ import random
 from PIL import Image
 import os
 from utils.Utils import letter_to_class
+from utils.Utils import letters
 from datetime import datetime
 from utils.Creator import get_new_plate
+import time
 
 
 def generate_and_save_plates(address, dataset_size: int = 200, img_size: tuple = (600, 400),
-                             save_bounding_boxes=True, save_mask=True, mask_state='grayscale'):
+                             save_plate=True, save_bounding_boxes=True,
+                             save_mask=True, save_glyphs=True, glyph_size: tuple = (128, 128),
+                             mask_state='grayscale'):
     random.seed(datetime.now())
-
     counter = 0
     for i in range(dataset_size):
         plate, perspective_plate, mask, bonding_boxes = get_new_plate(img_size, mask_state=mask_state)
+
         if save_bounding_boxes:
             if len(bonding_boxes) != 8:
                 counter += 1
@@ -29,6 +33,25 @@ def generate_and_save_plates(address, dataset_size: int = 200, img_size: tuple =
                 cv2.imshow('mask', mask)
                 cv2.waitKey()
                 continue
+            # Save Glyphs
+
+            if save_glyphs:
+                directory = address + "glyphs" + os.sep if opt.save_bounding_boxes & opt.save_glyphs else address
+                if not os.path.exists(directory):
+                    os.makedirs(directory)
+                for char in [str(i) for i in range(10)] + letters:
+                    if not os.path.exists(directory + char + os.sep):
+                        os.makedirs(directory + char + os.sep)
+                bonding_boxes.sort()
+                for box, char in zip(bonding_boxes, plate):
+                    x, y, w, h = box
+                    glyph = perspective_plate[y:y + h, x:x + w]
+                    _id = uuid.uuid4().__str__()
+                    glyph = cv2.cvtColor(glyph, cv2.COLOR_BGR2RGBA)
+                    glyph = Image.fromarray(glyph)
+                    glyph_template = Image.new('RGBA', (glyph_size[0], glyph_size[1]), (0, 0, 0, 0))
+                    glyph_template.paste(glyph, (0, 0))
+                    glyph_template.save(directory + char + os.sep + _id + ".png")
 
         perspective_plate = cv2.cvtColor(perspective_plate, cv2.COLOR_BGR2RGBA)
         perspective_plate = Image.fromarray(perspective_plate)
@@ -57,7 +80,8 @@ def generate_and_save_plates(address, dataset_size: int = 200, img_size: tuple =
             label_file.close()
 
         # Save plates
-        perspective_plate.save(directory + name + '$' + _id + ".png")
+        if save_plate:
+            perspective_plate.save(directory + name + '$' + _id + ".png")
 
         # Save masks
         if save_mask:
@@ -73,18 +97,23 @@ def generate_and_save_plates(address, dataset_size: int = 200, img_size: tuple =
 if __name__ == '__main__':
     # For test: set workers default to 1
     parser = argparse.ArgumentParser()
-    parser.add_argument('--size', type=int, default=10000, help='number of plates to generate')
+    parser.add_argument('--size', type=int, default=100, help='number of plates to generate')
     parser.add_argument('--workers', type=int, default=10, help='number of threads to run')
     parser.add_argument('--img_size', nargs='+', type=int, default=[500, 400], help='size of background')
+    parser.add_argument('--save_plate', action='store_true', help='save the masks if true')
     parser.add_argument('--save_bounding_boxes', action='store_true', help='save the bounding boxes if true')
     parser.add_argument('--save_mask', action='store_true', help='save the masks if true')
+    parser.add_argument('--save_glyphs', action='store_true', help='save the masks if true')
+    parser.add_argument('--glyph_size', nargs='+', type=int, default=[80, 80], help='size of saved glyphs')
     parser.add_argument('--mask_state', type=str, default='grayscale', help='grayscale or colorful')
-    parser.add_argument('--address', type=str, default='output/unet', help='The address of saving dataset')
+    parser.add_argument('--address', type=str, default='output/unet2', help='The address of saving dataset')
     opt = parser.parse_args()
     threadList = []
 
+    opt.save_plate = True
     opt.save_mask = False
     opt.save_bounding_boxes = True
+    opt.save_glyphs = True
     opt.mask_state = "grayscale"
 
     address = opt.address + os.sep if opt.address[-1] != os.sep else opt.address
@@ -97,16 +126,25 @@ if __name__ == '__main__':
     if not os.path.exists(directory):
         os.makedirs(directory)
 
+    directory = address + "glyphs" + os.sep if opt.save_bounding_boxes & opt.save_glyphs else address
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
     # if not os.path.exists(opt.address):
     #     os.makedirs(opt.address)
 
     size = opt.size
     max_threads = opt.workers
     for i in range(max_threads):
+        print("Tread ", i + 1, " is running")
         chunk_size = (size // max_threads) if i < max_threads - 1 else (size // max_threads) + (size % max_threads)
-        t = Thread(target=generate_and_save_plates, args=(opt.address, chunk_size, tuple(opt.img_size),
-                                                          opt.save_bounding_boxes, opt.save_mask, opt.mask_state))
+        t = Thread(target=generate_and_save_plates, args=(address, chunk_size, tuple(opt.img_size),
+                                                          opt.save_plate, opt.save_bounding_boxes,
+                                                          opt.save_mask, opt.save_glyphs, tuple(opt.glyph_size),
+                                                          opt.mask_state))
         t.start()
         threadList.append(t)
+        if i == 0:
+            time.sleep(4)
     for t in threadList:
         t.join()
--- synthetic_plates/readme.md
+++ synthetic_plates/readme.md
@@ -24,13 +24,13 @@ Generated Examples and Masks:<br/>
 In order to generate 20000 data using 10 threads for yolo, run this command:
 
 ```
-python3 DataMaker.py --size 20000 --workers 10 --img_size 500 400 --save_bounding_boxes --mask_state "grayscale" --address "output/yolo/train"
+python3 DataMaker.py --size 20000 --workers 10 --img_size 500 400 --save_plate --save_bounding_boxes --mask_state "grayscale" --address "output/yolo/train"
 
 ```
 Or:
 
 ```
-python3 DataMaker.py --size 20000 --workers 10 --img_size 500 400 --save_bounding_boxes --mask_state "grayscale" --address "output/yolo/test"
+python3 DataMaker.py --size 20000 --workers 10 --img_size 500 400 --save_plate --save_bounding_boxes --mask_state "grayscale" --address "output/yolo/test"
 
 ```
 
@@ -41,15 +41,23 @@ python3 DataMaker.py --size 20000 --workers 10 --img_size 500 400 --save_boundin
 In order to generate 5000 data using 10 threads for unet, run this command:
 
 ```
-python3 DataMaker.py --size 5000 --workers 10 --img_size 500 400 --save_mask --mask_state "colorful" --address "output/unet/train"
+python3 DataMaker.py --size 5000 --workers 10 --img_size 500 400 --save_plate --save_mask --mask_state "colorful" --address "output/unet/train"
 ```
 
 Or:
 
 ```
-python3 DataMaker.py --size 5000 --workers 10 --img_size 500 400 --save_mask --mask_state "colorful" --address "output/unet/test"
+python3 DataMaker.py --size 5000 --workers 10 --img_size 500 400 --save_plate --save_mask --mask_state "colorful" --address "output/unet/test"
 ```
 
+<h1>Glyphs</h1>
+<h3>Example</h3>
+
+In order to generate glyphs:
+
+```
+python3 DataMaker.py --size 5000 --workers 10 --img_size 500 400 --save_bounding_boxes --save_glyphs --glyph_size 80 80 --mask_state "grayscale" --address "output/yolo/train"
+```
 
 <h1>Analytics</h1>
 <h3>Example</h3>
