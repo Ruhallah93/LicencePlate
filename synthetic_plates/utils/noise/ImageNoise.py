6d8aee67ca96e6815ea519a5e853908bbc5ebed2
Mohammad Mahdi Bideh <mahdi.76911@gmail.com>
2022-01-19 23:20:02 +0330

    Mahdi: ImageNoise.py bug fix in RGBA (in paste 2 RGBA image, change transparency)  Code for checking:         if np.max(np.array(im_pil)[:, :, 3]) < 255:             plt.imshow(np.array(img))             plt.show()             plt.imshow(np.array(im_pil))             plt.show()             cv2.imshow('aa', np.array(im_pil))             cv2.waitKey()
            if np.max(img[:, :, 3]) < 255:
                print("wtf")

--- synthetic_plates/utils/noise/ImageNoise.py
+++ synthetic_plates/utils/noise/ImageNoise.py
@@ -2,6 +2,7 @@ from .Noise import Noise
 import cv2
 from PIL import Image
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 class ImageNoise(Noise):
@@ -11,14 +12,21 @@ class ImageNoise(Noise):
         self.plate_size = plate_size
 
     def apply(self, img):
-        # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGBA)
+        img = cv2.cvtColor(img, cv2.COLOR_RGBA2RGB)
         im_pil = Image.fromarray(img)
-        noise = Image.open(self.pathToImage).convert("RGBA").resize(self.plate_size)
+        noise = Image.open(self.pathToImage).resize(self.plate_size)
         im_pil.paste(noise, (0, 0), mask=noise)
+        # .convert("RGBA")
 
         # open_cv_image = np.array(im_pil)[:, :, :-1]
         # open_cv_image = open_cv_image[:, :, ::-1].copy()
-
         # open_cv_image = cv2.cvtColor(np.array(im_pil), cv2.COLOR_RGBA2BGRA)
 
-        return np.array(im_pil)
+        im_pil = cv2.cvtColor(np.array(im_pil), cv2.COLOR_RGB2RGBA)
+
+        # cv2.imshow('g', img)
+        # plt.imshow(np.array(im_pil))
+        # plt.show()
+        # cv2.waitKey()
+
+        return im_pil
